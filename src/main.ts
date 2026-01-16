229932693209ed248e4a3d016ef0e367a6b70fc3
Aron Balog <aronbalog@gmail.com>
2026-01-16 11:59:22 +0100

    Remove

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const port = process.env.PORT ?? 3001;
-  const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000';
   
   await app.listen(port, '0.0.0.0'); // <-- ensure 0.0.0.0
   console.log(`🚀 Backend server is running on: http://localhost:${port}`);
