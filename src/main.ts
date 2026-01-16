962952ea66f04e81cce196f3ec8af5bfa7e2bffa
Aron Balog <aronbalog@gmail.com>
2026-01-16 12:35:48 +0100

    +

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const port = process.env.PORT ?? 3001;
   
   await app.listen(port, '0.0.0.0'); // <-- ensure 0.0.0.0
-  console.log(`🚀 Backend server is running on: http://localhost:${port}`);
+  console.log(`🚀 Backend server is running on: http://0.0.0.0:${port}`);
 }
 bootstrap();
